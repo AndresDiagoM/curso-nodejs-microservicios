4cf2f1acfe84ce6dafd778c26136f40207c105f2
Andres Diago <andresdiag@unicauca.edu.co>
2023-11-12 09:03:05 -0500

    :construction: Class 8. Authentication

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,4 +1,5 @@
-const { nanoid } = require('nanoid');
+const nanoid = require('nanoid');
+const auth = require('../auth');
 
 const TABLA = 'user';
 
@@ -17,15 +18,25 @@ module.exports = function(injectedStore) {
         return store.get(TABLA, id);
     }
 
-    function upsert(body) {
+    async function upsert(body) {
         const user = {
             name: body.name,
+            username: body.username,
+            password: body.password,
         }
 
         if (body.id) {
             user.id = body.id;
         } else {
             user.id = nanoid();
+        }
+
+        if(body.password || body.username) {
+            await auth.upsert({
+                id: user.id,
+                username: user.username,
+                password: user.password,
+            });
         }
 
         return store.upsert(TABLA, user);
